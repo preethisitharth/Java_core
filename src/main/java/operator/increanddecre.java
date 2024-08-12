1b3845a68aca3748b93a33d08e71a63deebb0275
preethisitharth <preethisitharth@gmail.com>
2024-08-12 15:13:50 +0530

    operators

---
+++ src/main/java/operator/increanddecre.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class increanddecre {
+    public static void main(String[] args) {
+        //Pre increment
+        int cosmo = 200;
+        int alpha = 199;
+        System.out.println(++cosmo);
+        System.out.println(--alpha);
+        //Post Increment
+        System.out.println(cosmo++);
+        System.out.println(alpha--);
+
+    }
+}
---
+++ src/main/java/operator/logical.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class logical {
+    public static void main(String[] args) {
+        String district = "salem";
+        String district1 = "salem";
+        String district2 = "Yercaud";
+        System.out.println((district == district1) && (district1 == district2));
+        System.out.println((district == district1) || (district == district2));
+        int cosmno = 25;
+        int beta = 10;
+        System.out.println(!(cosmno == beta));
+
+    }
+}
