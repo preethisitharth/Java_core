304cc2a1c7addc6dfd956cf452a45fe3d2907076
preethisitharth <preethisitharth@gmail.com>
2024-08-12 15:24:30 +0530

    methods

---
+++ src/main/java/variables/demo_datatypes.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class demo_datatypes {
+    public static void main(String[] args) {
+        String Name="Preethi";
+        float Height=5.4f;
+        String Native_palce="Thiruvarur";
+        String CollegeName="Mahalingam College";
+        int age=21;
+        long contactnumber=9785461234l;
+        char Gender='F';
+
+        System.out.println("..........Student_data...........");
+        System.out.println("My name is :"+Name);
+        System.out.println("Height is :"+Height);
+        System.out.println("Native Place is : "+Native_palce);
+        System.out.println("College Name :"+CollegeName);
+        System.out.println("Age is "+age);
+        System.out.println("Contact Number:"+contactnumber);
+        System.out.println("Gender:"+Gender);
+
+
+    }
+}
+
