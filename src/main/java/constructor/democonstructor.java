da0066bb8343c16176097901725206bce6888870
preethisitharth <preethisitharth@gmail.com>
2024-09-03 11:46:21 +0530

    Constructor

---
+++ src/main/java/constructor/democonstructor.java
@@ -0,0 +1,21 @@
+package constructor;
+
+public class democonstructor{
+
+        public void hello()
+        {
+            System.out.println("Hello There.....");
+        }
+        public democonstructor()
+        {
+            System.out.println("This is Default Constructor");
+        }
+        public static void main(String[] args) {
+            //classname objectname= new constructorname();
+            democonstructor dc= new democonstructor();
+
+        }
+
+    }
+
+
